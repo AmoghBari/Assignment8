80c2b8f4a39927c2428c95cf6a039331959cc6f0
AmoghBari <145555795+AmoghBari@users.noreply.github.com>
2023-12-01 16:10:44 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,38 +4,17 @@ pipeline {
     environment {
         AWS_REGION = 'ap-south-1'
     }
-    
-    stages {
-        stage('Terraform Init') {
+  stages {
+        stage('Init/Plan/Apply') {
             steps {
                 script {
-                    sh 'terraform init'
-                }
-            }
-        }
-
-        stage('Terraform Plan') {
-            steps {
-                script {
-                    sh 'terraform plan'
-                }
-            }
-        }
-        
-        stage('Terraform Apply') {
-            steps {
-                script {
-                    sh 'terraform apply -auto-approve'
+                    withAWS(region: AWS_REGION, credentials: 'MyAWSCredentials') {
+                        sh 'terraform init'
+                        sh 'terraform plan'
+                        sh 'terraform apply -auto-approve'
+                    }
                 }
             }
         }
     }
-
-            // stage('Terraform Destroy') {
-            //     steps {
-            //         script  {
-            //             sh 'terraform destroy -auto-approve'
-            //         }
-            //     }
-            // }
-        }
+}
