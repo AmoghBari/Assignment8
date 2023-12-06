54ab94ad2008cb455f2063d52afd6476966c324d
AmoghBari <145555795+AmoghBari@users.noreply.github.com>
2023-12-06 14:52:15 +0530

    Update output.tf

--- modules/ssh/output.tf
+++ modules/ssh/output.tf
@@ -10,6 +10,6 @@ output "key_pair" {
   value = aws_key_pair.ec2_key_pair.public_key
 }
 
-output "private_key_pem" {
-  value = tls_private_key.ssh_key.private_key_pem
-}
+#output "private_key_pem" {
+  #value = tls_private_key.ssh_key.private_key_pem
+#}
