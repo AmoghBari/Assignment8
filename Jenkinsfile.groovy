ebc043289403774b99a9d06870acf5b7047e4c25
AmoghBari <145555795+AmoghBari@users.noreply.github.com>
2023-12-06 15:42:26 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -14,22 +14,18 @@ pipeline {
             }
         }
 
+        stage('Terraform Plan') {
+            steps {
+                script {
+                    sh 'terraform plan'
+                }
+            }
+        }
+
         stage('Terraform Apply') {
             steps {
                 script {
                     sh 'terraform apply -auto-approve'
-                }
-            }
-        }
-    }
-
-    post {
-        always {
-            stage('Terraform Destroy') {
-                steps {
-                    script {
-                        sh 'terraform destroy -auto-approve'
-                    }
                 }
             }
         }
